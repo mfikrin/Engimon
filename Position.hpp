3ddbdc69cd63a909595a04558fa1e9aa667d0100
naufalizza <mail.naufalizza@gmail.com>
2021-03-14 10:32:30 +0700

    init class Position

---
+++ Position.hpp
@@ -0,0 +1,18 @@
+#ifndef _Position_hpp_
+#define _Position_hpp_
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Position{
+    protected:
+        int x;
+        int y;
+        // Generic class entities
+    public:
+        
+}
+
+#endif // _Position_hpp_
