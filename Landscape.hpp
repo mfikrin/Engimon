fa770e94eb03a6d0cda6365dd0b728ed26844056
raisvaza <raisvaza@gmail.com>
2021-03-20 23:49:45 +0700

    add Landscape.hpp

---
+++ Landscape.hpp
@@ -0,0 +1,46 @@
+#ifndef LANDSCAPE__HPP
+#define LANDSCAPE__HPP
+#include "LandscapeType.hpp"
+#include "Engimon.hpp"
+#include <stdlib.h>
+
+class Landscape {
+    private:
+        LandscapeType type;
+        Engimon engimonLiar;
+
+    public:
+        Landscape(LandscapeType type) {
+            this->type = type;
+        }
+
+        void SpawnEngimon(vector<vector<Engimon>> Ensiklopedia) {
+            if (this->type == LandscapeType::Grassland)
+            {
+                int randomElement = rand() % 5;
+                
+                while (randomElement == 1 || randomElement == 4) // KALAU NO ELEMENT GIMANA??
+                {
+                    randomElement = rand() % 5;
+                }
+                
+                int randomEngimon = rand() % Engimon[randomElement].size();
+                engimonLiar = Ensiklopedia[randomElement][randomEngimon];
+
+                
+            } else // (this->type == LandscapeType::Sea)
+            {
+                int randomElement = rand() % 5;
+                while (randomElement == 0 || randomElement == 2 || randomElement == 3) // KALAU NO ELEMENT GIMANA??
+                {
+                    randomElement = rand() % 5;
+                }
+            }
+            
+            int randomEngimon = rand() % Engimon[randomElement].size();
+                engimonLiar = Ensiklopedia[randomElement][randomEngimon];
+            
+        }
+};
+
+#endif
